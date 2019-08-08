3be53da0cb6abff64d6ead92f5afcf26bd9947b4
Yimmi-Gil-UD <jimmi_gil@hotmail.es>
2019-08-08 14:28:33 -0500

    creacion del primer servicio

---
+++ Samtel.Application/ApplicationServices/DTOs/CompanyDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samtel.Application.ApplicationServices.DTOs
+{
+    public class CompanyDTO
+    {
+        public string Nit { get; set; }
+        public string NombreEmpresa { get; set; }
+    }
+}
---
+++ Samtel.Persistence/BusinessServiceProvider/ClientRepository.cs
@@ -0,0 +1,29 @@
+﻿using Samtel.Application;
+using Samtel.Application.BusinessService;
+using Samtel.Application.BusinessService.Base;
+using Samtel.Domain.Models.Entities;
+using Samtel.Persistence.BusinessServiceProvider.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samtel.Persistence.BusinessServiceProvider
+{
+    public class ClientRepository : RepositoryBase<Client>, IClientRepository
+    {
+        private readonly RepositoryBase<Client> _RepositoryBase;
+
+        public ClientRepository(IContext context, RequestContext requestContext): base(context, requestContext)
+        {
+        }
+
+        public List<Client> listClients()
+        {
+            var testing = Query<Client>("SELECT * FROM BIG_CLIENTES_TEMP  ").ToList();
+            // _cacheService.Add(keyCache, paymentMethods);
+            return testing;
+        }
+    }
+}
